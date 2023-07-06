3016d142fca1779de03656cdf2f44330ec51b047
Kevin Chandra <kacol.bot@gmail.com>
2023-07-06 18:59:08 +0700

    install lunar

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Lunar\Base\Traits\LunarUser;
 
 class User extends Authenticatable
 {
+    use LunarUser;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
